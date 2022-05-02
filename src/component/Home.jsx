fca4489d5a2cc2a31244c61ba1effc31e0c22a26
Masrur Alam <masrur.jsr@gmail.com>
2022-05-02 15:32:56 +0530

    committing @3:32

---
+++ src/component/Home.jsx
@@ -0,0 +1,7 @@
+
+
+export const Home = ()=> {
+    return <div>
+        <h2>Welcome to Laptop service center. Please login to continue</h2>
+    </div>
+}
---
+++ src/component/Login.jsx
@@ -0,0 +1,8 @@
+
+export const Login = ()=> {
+    return <div>
+        <input type="text" placeholder="Enter Username" />
+        <input type="text" placeholder="Enter password" />
+        <button >Login</button>
+    </div>
+}
